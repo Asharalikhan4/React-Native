57b3ac6e0fd5084e35d97620be2f3ab182cc9078
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-07-05 22:43:17 +0530

    Custom button added

--- 04DivingDeeper/screens/StartGameScreen.js
+++ 04DivingDeeper/screens/StartGameScreen.js
@@ -1,9 +1,12 @@
 import { View, TextInput } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
     return (
         <View>
             <TextInput />
+            <PrimaryButton>RESET</PrimaryButton>
+            <PrimaryButton>CONFIRM</PrimaryButton>
         </View>
     );
 };
