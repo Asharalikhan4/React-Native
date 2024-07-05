dfd0946ab5ca39c8814d453faee00ddd19365eb4
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-07-05 22:51:42 +0530

    Shadow added

--- 04DivingDeeper/screens/StartGameScreen.js
+++ 04DivingDeeper/screens/StartGameScreen.js
@@ -1,9 +1,24 @@
-import { View, TextInput } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginTop: 100,
+        marginHorizontal: 24,
+        padding: 16,
+        backgroundColor: "#72063c",
+        borderRadius: 8,
+        elevation: 8,
+        shadowColor: "black",
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25,
+    }
+});
 
 const StartGameScreen = () => {
     return (
-        <View>
+        <View style={styles.inputContainer}>
             <TextInput />
             <PrimaryButton>RESET</PrimaryButton>
             <PrimaryButton>CONFIRM</PrimaryButton>
