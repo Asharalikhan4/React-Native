484646e9da571a7b1a51c23360f0a34254590073
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-07-06 07:15:40 +0530

    Background Image added

--- 04DivingDeeper/App.js
+++ 04DivingDeeper/App.js
@@ -1,17 +1,22 @@
-import { View, StyleSheet } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-  }
+  },
+  baclgroundImage: {
+    opacity: 0.15,
+  },
 });
 
 export default function App() {
   return (
     <LinearGradient colors={["#4e0329","#ddb52f"]} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground resizeMode="cover" style={styles.rootScreen} source={require("./assets/splash.png")} imageStyle={styles.baclgroundImage}>
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 };
